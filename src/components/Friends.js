944ac4f2474cfab916a235293acc8adae93bea14
Emman <emman199810@gmail.com>
2021-02-20 15:41:11 +0800

    adding search functionality

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -3,11 +3,13 @@ import { Button, Form, FormText, Row } from 'react-bootstrap';
 import { FaAngleLeft, FaWindowClose } from 'react-icons/fa';
 import FriendItem from './FriendItem';
 import './Pages/styles/friendsStyle.css'
-import { useEffect } from "react";
+import { useEffect, useState} from "react";
+import { fetchRecipient } from '../queries/UserQuery';
 const USER_API = 'https://serene-hamlet-21553.herokuapp.com/user/'
 
 const Friends = ({displayType, CloseMe, setRoom}) => {
     const [friends, setFriends] = React.useState([])
+    const [searchtext, setSearchtext] = useState('');
     useEffect(() => {
         const getFriends = async () => {
             const friends_result = await fetchFriends()
@@ -15,6 +17,18 @@ const Friends = ({displayType, CloseMe, setRoom}) => {
         }
         getFriends()
     }, []);
+    useEffect(() => {
+      if(searchtext.length>0){
+            searchUser(searchtext)
+      }else{
+        const getFriends = async () => {
+            const friends_result = await fetchFriends()
+            setFriends(friends_result)
+        }
+        getFriends()
+      }
+        
+    }, [searchtext]);
 
     const fetchFriends = async () => {
         const friends = await fetch(USER_API+'fetch')
@@ -24,16 +38,33 @@ const Friends = ({displayType, CloseMe, setRoom}) => {
     const setChatRoom = (friend) => {
         setRoom(friend)
     }
+    const searchFriends = (e)=> {
+        setSearchtext(e.target.value)
+    }
+    const searchUser = async(fullname) => {
+        const users = await fetch(USER_API+'search/'+fullname)
+        if(users.ok){
+            const result = await users.json()
+            setFriends(result)
+            return true
+        }else{
+            console.log(users.status)
+            return false
+        }
+    }
     return (
         <div className = {displayType? 'friends-div': 'hide'}>
             <Button className='btn-back' variant = 'outline' size='sm' onClick = {()=>{CloseMe()}}><FaWindowClose/></Button>
             <div className = 'friends-header'>
                 <h6>To :</h6>
                 <input type = 'text' className='search-control'
-                placeholder = 'Type the name of the person'/>
+                placeholder = 'Type the name of the person'
+                onChange = {(e)=> {searchFriends(e)}}
+                value = {searchtext}/>
             </div>
             <hr></hr>
             <div className = 'friend-list-div'>
+                {friends.length===0?<center>No results</center>:''}
                {friends.map((friend, id)=>(
                    <FriendItem friend = {friend} key = {id} setRoom = {setChatRoom}/>
                ))}
